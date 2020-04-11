b6da8c364074f25817edb738816215240c8fed32
Bus1k <36888046+Bus1k@users.noreply.github.com>
2020-04-11 17:33:55 +0200

    first controller

---
+++ App/Controllers/Posts.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Posts
+{
+    public function index()
+    {
+        echo 'Post Controller - test index';
+    }
+
+    public function addNew()
+    {
+        echo 'Post Controller - test addNew';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,29 +1,29 @@
 <?php
 
+require '../App/Controllers/Posts.php';
 require '../Core/Router.php';
 
 $router = new Router();
 
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
-$router->add('admin/{controller}/{action}');
 
-echo '<pre>';
-var_dump($router->getRoutes());
-echo '<pre>';
+//echo '<pre>';
+//var_dump($router->getRoutes());
+//echo '<pre>';
 
 $url = $_SERVER['QUERY_STRING'];
 
-if($router->match($url))
-{
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '<pre>';
-}
-else
-{
-    echo 'No found route URL: '.$url;
-}
+//if($router->match($url))
+//{
+//    echo '<pre>';
+//    var_dump($router->getParams());
+//    echo '<pre>';
+//}
+//else
+//{
+//    echo 'No found route URL: '.$url;
+//}
+
+$router->dispatch($url);
